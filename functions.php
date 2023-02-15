65d22a9510f1c96023397085ddbdd974be929fd2
Karen Yazmin <karen.juarez@devitm.com>
2023-02-15 16:46:19 -0600

    Creando el Footer y cargando Scripts

--- functions.php
+++ functions.php
@@ -5,9 +5,14 @@
  */
 
  function edc_scripts(){
-    /** Scripts */
-    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/boostrap.css', false, '4.1.3');
+    /** Styles */
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.css', false, '4.1.3');
 
     wp_enqueue_style('style', get_stylesheet_uri(), array('bootstrap-css'));
+
+    /** Scripts */
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('popper', get_template_directory_uri() . '/js/popper.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.js', array('popper'), '1.0', true);
  }
  add_action('wp_enqueue_scripts', 'edc_scripts');
